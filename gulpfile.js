1199a6d86baf84f7747972c139eb3b4013cb36b0
Leonardo Faria <leonardofaria@gmail.com>
2016-06-11 18:39:23 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp'), 
     sass = require('gulp-sass') ,
-    uglify = require('gulp-uglifyjs'),
     browserSync = require('browser-sync').create(),
     rsync = require('rsyncwrapper').rsync;
 
 var config = {
   themePath: './wp-content/themes/blankslate',
   sassPath: './wp-content/themes/blankslate/resources/sass',
-  jsPath: './wp-content/themes/blankslate/resources/js',
-  dest: './wp-content/themes/blankslate'
 };
 
 gulp.task('serve', function() {
@@ -17,16 +14,10 @@ gulp.task('serve', function() {
   });
 });
 
-gulp.task('js', function() {
-  return gulp.src(config.jsPath + '/app.js')
-    .pipe(uglify())
-    .pipe(gulp.dest(config.dest));
-});
-
 gulp.task('css', function () {
   return gulp.src(config.sassPath + '/app.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulp.dest(config.themePath));
 });
 
 gulp.task('rsync', function() {
@@ -37,9 +28,8 @@ gulp.task('rsync', function() {
 });
 
  gulp.task('watch', function() {
-  gulp.watch(config.themePath + '/**/*.php').on('change', browserSync.reload);
+  gulp.watch(config.themePath + '/**/*.php', browserSync.reload);
   gulp.watch(config.sassPath + '/**/*.scss', ['css', browserSync.reload]); 
-  gulp.watch(config.jsPath + '/**/*.js', ['js', browserSync.reload]); 
 });
 
   gulp.task('default', ['serve', 'watch']);
